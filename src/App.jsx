4f7a3e18aa8ef532ceb28dbac09e50f3439dced2
Excel coder <www.chibuikeexcel@gmail.com>
2025-04-26 02:40:24 +0200

    Initial commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Hero from "./sections/Hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
+import FeatureCard from "./sections/FeatureCard.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
+import Testimonials from "./sections/Testimoniald.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+const App = () => {
+  return (
+   <>
+   <NavBar/>
+   <Hero/>
+   <ShowcaseSection/>
+       <LogoSection/>
+       <FeatureCard/>
+       <ExperienceSection/>
+       <TechStack/>
+       <Testimonials/>
+       <Contact/>
+       <Footer/>
+   </>
+  )
+}
+
+export default App
